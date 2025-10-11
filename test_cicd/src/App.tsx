94b10566730307e1280a314156e4d8165de5746a
george ibrahim hanif <122005621+JorLojor@users.noreply.github.com>
2025-10-11 19:03:37 +0700

    const dor server

--- test_cicd/src/App.tsx
+++ test_cicd/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>cuman test ci cd doang kok</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
